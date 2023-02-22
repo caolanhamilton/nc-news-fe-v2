20e63e40946e646666a8401245a0fcea111e375f
Caolan <caolanhamilton75@gmail.com>
2023-02-22 13:10:37 +0000

    Create article voter component

--- src/pages/components/ArticleSubheadings.tsx
+++ src/pages/components/ArticleSubheadings.tsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
-import type { Article, PatchArticleVotes } from "../../../types";
-import { patchArticleVotes } from "../../apiCalls/updateArticleVotes";
-import UpVoteButton from "./UpVoteButton";
-import DownVoteBtn from "./DownVoteButton";
+import type { Article } from "../../../types";
+import ArticleVoter from "./ArticleVoter";
 
 type Props = {
   article: Article;
@@ -11,31 +8,14 @@ type Props = {
 // get original votes on load
 
 export default function ArticleSubheadings({ article }: Props) {
-  const [likeChanges, setLikeChanges] = useState(0);
-
   return (
     <div className="flex items-center justify-between space-x-8">
       <div className="flex items-center space-x-2">
         <div className="w-24 rounded-full border border-orange-500 bg-orange-300 p-1 text-center uppercase">
           <p className="text-white">{article.topic}</p>
         </div>
-        <div className="flex w-24 flex-row justify-evenly space-x-2 rounded-lg bg-slate-800 p-1">
-          <DownVoteBtn
-            setLikeChanges={setLikeChanges}
-            likeChanges={likeChanges}
-            ids={{ articleId: article.article_id, commentId: -1 }}
-            patchVoteFunc={patchArticleVotes as PatchArticleVotes}
-          ></DownVoteBtn>
-          <p className="text-white">{article.votes + likeChanges}</p>
-          <UpVoteButton
-            setLikeChanges={setLikeChanges}
-            likeChanges={likeChanges}
-            ids={{ articleId: article.article_id, commentId: -1 }}
-            patchVoteFunc={patchArticleVotes as PatchArticleVotes}
-          ></UpVoteButton>
-        </div>
+        <ArticleVoter article={article} />
       </div>
-
       <div className="font-semibold italic text-gray-400">
         <p>
           Posted {article.created_at.toString().split("T")[0]} by{" "}
---
+++ src/pages/components/ArticleVoter.tsx
@@ -0,0 +1,31 @@
+import {useState} from "react";
+import DownVoteBtn from "./DownVoteButton";
+import UpVoteButton from "./UpVoteButton";
+import type { Article, PatchArticleVotes } from "../../../types";
+import { patchArticleVotes } from "../../apiCalls/updateArticleVotes";
+
+type Props = {
+    article: Article;
+};
+
+export default function ArticleVoter({article}: Props) {
+  const [likeChanges, setLikeChanges] = useState(0);
+
+  return (
+    <div className="flex w-24 flex-row justify-evenly space-x-2 rounded-lg bg-slate-800 p-1">
+      <DownVoteBtn
+        setLikeChanges={setLikeChanges}
+        likeChanges={likeChanges}
+        ids={{ articleId: article.article_id, commentId: -1 }}
+        patchVoteFunc={patchArticleVotes as PatchArticleVotes}
+      ></DownVoteBtn>
+      <p className="text-white">{article.votes + likeChanges}</p>
+      <UpVoteButton
+        setLikeChanges={setLikeChanges}
+        likeChanges={likeChanges}
+        ids={{ articleId: article.article_id, commentId: -1 }}
+        patchVoteFunc={patchArticleVotes as PatchArticleVotes}
+      ></UpVoteButton>
+    </div>
+  );
+}
