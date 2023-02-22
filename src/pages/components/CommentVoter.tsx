289448ab7494b841b30c5217079cd1e2bd4bc994
Caolan <caolanhamilton75@gmail.com>
2023-02-22 12:42:44 +0000

    Create comment voter

---
+++ src/pages/components/CommentVoter.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Comment } from "../../../types";
+import { patchCommentVotes } from "../../apiCalls/updateCommentVotes";
+import UpVoteButton from "./UpVoteButton";
+import DownVoteBtn from "./DownVoteButton";
+
+
+type Props = {
+  comment: Comment;
+  articleId: number;
+};
+
+export default function CommentVoter({ comment, articleId }: Props) {
+  const [likeChanges, setLikeChanges] = useState(0);
+  return (
+    <div className="flex h-24 flex-col justify-center space-y-2 rounded-lg bg-slate-800 text-center">
+      <UpVoteButton
+        setLikeChanges={setLikeChanges}
+        ids={{ commentId: comment.comment_id, articleId: articleId }}
+        likeChanges={likeChanges}
+        patchVoteFunc={patchCommentVotes}
+      ></UpVoteButton>
+      <p>{comment.votes + likeChanges}</p>
+      <DownVoteBtn
+        setLikeChanges={setLikeChanges}
+        ids={{ commentId: comment.comment_id, articleId: articleId }}
+        likeChanges={likeChanges}
+        patchVoteFunc={patchCommentVotes}
+      ></DownVoteBtn>
+    </div>
+  );
+}
